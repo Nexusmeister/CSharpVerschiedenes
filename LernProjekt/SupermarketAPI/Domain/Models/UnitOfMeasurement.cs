72d46ff559ff6e206ffbca4fbec5097b5aa77c30
Kaltenbach, Robin <robin@cskaltenbach.de>
2020-08-06 22:51:59 +0200

    Initial Lernprojekt

---
+++ LernProjekt/SupermarketAPI/Domain/Models/UnitOfMeasurement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Supermarket.API.Domain.Models
+{
+    public enum UnitOfMeasurement : byte
+    {
+        [Description("UN")]
+        Unity = 1,
+
+        [Description("MG")]
+        Milligramm = 2,
+
+        [Description("G")]
+        Gramm = 3,
+
+        [Description("KG")]
+        Kilogramm = 4,
+
+        [Description("L")]
+        Liter = 5
+    }
+}
---
+++ LernProjekt/SupermarketAPI/Extensions/ModelStateExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Supermarket.API.Extensions
+{
+    public static class ModelStateExtensions
+    {
+        public static IEnumerable<string> GetErrorMessages(this ModelStateDictionary dictionary)
+        {
+            return dictionary.SelectMany(x => x.Value.Errors).Select(y => y.ErrorMessage).ToList();
+        }
+    }
+}
